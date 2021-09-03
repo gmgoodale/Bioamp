a84ba50b7192f9a19d035ff4c24e5594ad06b9a2
Garett Goodale <garettgoodale@Garetts-MacBook-Pro.local>
2021-09-03 10:20:18 -0400

    Pong example

--- DataFromDAQ.py
+++ DataFromDAQ.py
@@ -2,8 +2,9 @@
 import nidaqmx
 import time
 
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+class DAQ(port):
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
 
     while(True):
         print(task.read())
